99b6b2a9b1f6968701e24abedf70b64d3f8d5fcd
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-02 09:12:33 +0700

    add employee swagger

---
+++ BookingManagementApp/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _employeeRepository.GetAll();
+            if (!result.Any())
+            {
+                return NotFound("Data Not Found");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var result = _employeeRepository.GetByGuid(guid);
+            if (result is null)
+            {
+                return NotFound("Id Not Found");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employees employees)
+        {
+            var result = _employeeRepository.Create(employees);
+            if (result is null)
+            {
+                return BadRequest("Failed to create data");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPut("{guid}")]
+        public IActionResult Update(Guid guid, [FromBody] Employees updatedEmployee)
+        {
+            var existingEmployee = _employeeRepository.GetByGuid(guid); ;
+            if (existingEmployee is null)
+            {
+                return NotFound("Id Not Found");
+            }
+
+            existingEmployee.Nik = updatedEmployee.Nik;
+            existingEmployee.FirstName = updatedEmployee.FirstName;
+            existingEmployee.LastName = updatedEmployee.LastName;
+            existingEmployee.BirthDate = updatedEmployee.BirthDate;
+            existingEmployee.Gender = updatedEmployee.Gender;
+            existingEmployee.HiringDate = updatedEmployee.HiringDate;
+            existingEmployee.Email = updatedEmployee.Email;
+            existingEmployee.PhoneNumber = updatedEmployee.PhoneNumber;
+            existingEmployee.ModifiedeDate = updatedEmployee.ModifiedeDate;
+
+
+            var result = _employeeRepository.Update(existingEmployee);
+            if (!result)
+            {
+                return BadRequest("Failed to update data");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{guid}")]
+        public IActionResult Delete(Guid guid)
+        {
+            var existingEmployee = _employeeRepository.GetByGuid(guid); ;
+            if (existingEmployee is null)
+            {
+                return NotFound("Id Not Found");
+            }
+
+            var result = _employeeRepository.Delete(existingEmployee);
+            if (!result)
+            {
+                return NotFound("Delete failed");
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ BookingManagementApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,68 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly BookingManagementDbContext _context;
+
+        public EmployeeRepository(BookingManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employees> GetAll()
+        {
+            return _context.Set<Employees>().ToList();
+        }
+
+        public Employees? GetByGuid(Guid guid)
+        {
+            return _context.Set<Employees>().Find(guid);
+        }
+
+        public Employees? Create(Employees employees)
+        {
+            try
+            {
+                _context.Set<Employees>().Add(employees);
+                _context.SaveChanges();
+                return employees;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Update(Employees employees)
+        {
+            try
+            {
+                _context.Set<Employees>().Update(employees);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Employees employees)
+        {
+            try
+            {
+                _context.Set<Employees>().Remove(employees);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
