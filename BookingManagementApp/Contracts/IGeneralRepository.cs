993b4b765b56b4ef3c3d5506b67d8f2c98b1057f
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-02 16:44:04 +0700

    add general repository

---
+++ BookingManagementApp/Contracts/IGeneralRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    /* 
+     * Generic class digunakan untuk membuat interface untuk semua repository
+     * 
+     * TEntity => digunakan untuk menentukan tipe objek/class yang akan digunakan
+     */
+    public interface IGeneralRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity? GetByGuid(Guid guid);
+        TEntity? Create(TEntity entity);
+        bool Update(TEntity entity);
+        bool Delete(TEntity entity);
+    }
+}
