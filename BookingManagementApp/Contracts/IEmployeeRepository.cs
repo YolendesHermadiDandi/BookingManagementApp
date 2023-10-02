d3d0d49f71410d9ae33069728074d0addf433fcd
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-02 09:12:54 +0700

    add emp repository

---
+++ BookingManagementApp/Contracts/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employees> GetAll();
+        Employees? GetByGuid(Guid guid);
+        Employees? Create(Employees employees);
+        bool Update(Employees employees);
+        bool Delete(Employees employees);
+    }
+}
