27e81d88303d5436fb4c4d8bb99d730c6648de46
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-03 07:57:50 +0700

    add enum comment

--- BookingManagementApp/Models/GlobalMathod.cs
+++ BookingManagementApp/Models/GlobalMathod.cs
@@ -3,13 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Models
 {
+    /* 
+   * Generic class digunakan untuk membuat parameter/method untuk 
+   * nama model dan tipedata yang sama 
+   * dan diterapkan ke semua mode
+   * class ini juga merupakan class parent dari semua class model
+   * 
+   * 
+   */
     public abstract class GlobalMathod
     {
 
         [Key, Column("guid")]
         public Guid Guid { get; set; }
         [Column("create_date")]
-        public DateTime CreateDate { get; set; }
+        public DateTime CreateDate { get; set;a }
         [Column("modified_date")]
         public DateTime ModifiedeDate { get; set; }
     }
--- BookingManagementApp/Utilities/Enums/StatusLevel.cs
+++ BookingManagementApp/Utilities/Enums/StatusLevel.cs
@@ -2,6 +2,10 @@
 
 namespace API.Utilities.Enums
 {
+    /*
+    * pembuatan emun untuk status level
+    * dengan reprentasi menggunakan angka dimulai dari 0
+    */
     public enum StatusLevel
     {
         Requested,
