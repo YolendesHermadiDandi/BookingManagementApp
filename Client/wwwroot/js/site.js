063074ad51c5cf9d1fdd6ada93d1547c007b9f5b
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-11 23:36:09 +0700

    Add Pokemon API

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/",
+    //success: (result) => {
+    //    console.log(result);
+    //}
+}).done((result) => {
+    console.log(result)
+    let temp = "";
+    $.each(result.results, (key, val) => {
+        temp += `<tr>
+                    <td>${key + 1}</td>
+                    <td>${val.name}</td>
+                    <td><button type="button" onclick="detail('${val.url}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalPoke">Detail</button></td>
+                </tr >`;
+    });
+    $("#tbodyPoke").html(temp);
 
-// Write your JavaScript code.
+
+}).fail((error) => {
+    console.log(error);
+})
+
+function detail(stringUrl) {
+    $.ajax({
+        url: stringUrl
+    }).done((res) => {
+        $(".modal-title").html(res.name);
+    })
+}
